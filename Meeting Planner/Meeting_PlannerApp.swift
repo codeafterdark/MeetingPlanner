bc0a7f183df4a78688a1193b3051dc2db7da5cde
AJL <al.lowenstein@icloud.com>
2026-02-07 18:12:44 -0500

    Initial Commit

---
+++ Meeting Planner/Meeting_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_PlannerApp.swift
+//  Meeting Planner
+//
+//  Created by Alan Lowenstein on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Meeting_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meeting PlannerTests/Meeting_PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_PlannerTests.swift
+//  Meeting PlannerTests
+//
+//  Created by Alan Lowenstein on 2/7/26.
+//
+
+import Testing
+@testable import Meeting_Planner
+
+struct Meeting_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
